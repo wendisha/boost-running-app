f1474d42b538c85238b9567ee3f934cb2c515a74
sarahpaz <sarah.paz@gmail.com>
2019-07-04 00:17:22 -0400

    add index, new and create actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user.save && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      redirect_to root_path
+    end
+  end
+end
