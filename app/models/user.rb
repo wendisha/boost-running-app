e29ab9a4818cd24f64cc23a8edcfb5f331aebf5b
sarahpaz <sarah.paz@gmail.com>
2019-07-04 10:28:21 -0400

    add email and username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  
   has_many :runs
   has_many :charities, through: :runs
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 
   has_secure_password
+
 end
