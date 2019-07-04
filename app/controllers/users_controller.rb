3a7ca000b6026c5e52ce49eb25985e3e5dd2ab73
sarahpaz <sarah.paz@gmail.com>
2019-07-04 01:20:30 -0400

    add new and create actions, add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
