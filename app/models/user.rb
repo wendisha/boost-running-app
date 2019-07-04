5c910234b2a493e3bc084bbf6cdff5f8f9ebc9af
sarahpaz <sarah.paz@gmail.com>
2019-07-04 01:20:56 -0400

    add username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :runs
   has_many :charities, through: :runs
+  validates :username, presence: true
+  validates :email, presence: true
 
   has_secure_password
 end
