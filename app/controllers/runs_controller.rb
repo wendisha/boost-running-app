c8f8dc5324b8457af9088fc7421b87422d0eb025
sarahpaz <sarah.paz@gmail.com>
2019-07-05 00:57:38 -0400

    add run association to user in create action

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -3,5 +3,28 @@ class RunsController < ApplicationController
   def index
     @runs = Run.all
   end
-  
+
+  def show
+    @run = Run.find_by(id: params[:id])
+  end
+
+  def new
+    @run = Run.new
+  end
+
+  def create
+    @run = Run.new(run_params)
+    @run.user_id = current_user.id
+    if @run.save
+      redirect_to run_path(@run.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def run_params
+    params.require(:run).permit(:distance, :duration, :location, :shoes, :notes, :charity_id)
+  end
 end
