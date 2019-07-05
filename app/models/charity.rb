7b58a13689ae8dc9e6e12be3843b23e57d319774
sarahpaz <sarah.paz@gmail.com>
2019-07-04 22:31:37 -0400

    add custom not_a_duplicate validation

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,5 +2,11 @@ class Charity < ApplicationRecord
   has_many :runs
   has_many :users, through: :runs
 
-  validates :name, presence: true
+  validates :name, :description, presence: true
+  validate :not_a_duplicate, on: :create
+  
+  def not_a_duplicate
+    errors.add(:name, "is already a listed charity.") if Charity.find_by(name: name)
+
+  end
 end
