fd7bbb0ee701d0a7d92ac63640e3fc3f4fff0267
sarahpaz <sarah.paz@gmail.com>
2019-07-04 20:37:57 -0400

    add new and create actions and charity_params

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -4,4 +4,26 @@ class CharitiesController < ApplicationController
     @charity = Charity.find_by(id: params[:id])
   end
 
+  def index
+    @charities = Charity.all
+  end
+
+  def new
+    @charity = Charity.new
+  end
+
+  def create
+    @charity = Charity.new(charity_params) 
+    if @charity.save
+      redirect_to charity_path(@charity.id)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def charity_params
+    params.require(:charity).permit(:name, :description)
+  end
 end
