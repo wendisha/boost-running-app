b28c7cda0bd59b82253ccd080354c44bbb2ec64f
sarahpaz <sarah.paz@gmail.com>
2019-07-04 20:47:55 -0400

    remove helper methods and move to application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!@current_user
-  end
-
 end
