c11bc1e3c0d0e2acec5f38b9391795f59ad7d35a
sarahpaz <sarah.paz@gmail.com>
2019-07-04 20:44:18 -0400

    add logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,5 +23,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   delete '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
