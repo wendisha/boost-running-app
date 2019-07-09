24d4e39ce79c97818697e8b60643b741f21d9865
sarahpaz <sarah.paz@gmail.com>
2019-07-09 13:53:52 -0700

    add charity_attributes

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,4 +3,10 @@ class Run < ApplicationRecord
   belongs_to :charity
 
   validates :distance, :duration, :charity_id, presence: true
+
+  def charity_attributes=(attributes)
+    self.charity = Charity.find_or_create_by(attributes) if !attributes['name'].empty?
+    self.charity
+  end
+
 end
