de9ab90bc1cd22d22c5b9c7c57631fa4a7115108
sarahpaz <sarah.paz@gmail.com>
2019-07-16 01:24:56 -0400

    add method to create new user account through omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.password = SecureRandom.hex
+    end
+  end
 end
