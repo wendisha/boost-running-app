fa345b97838cad6775bc868cd2e417932f8f3c33
sarahpaz <sarah.paz@gmail.com>
2019-07-16 01:23:34 -0400

    add omniauth action and request for auth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,16 @@ class SessionsController < ApplicationController
   end
 
   def omniauth
-    byebug
+    @user = User.from_omniauth(auth)
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
   
 end
