d38f0fd21e95e884a912fb8828530b899dc53a3a
sarahpaz <sarah.paz@gmail.com>
2019-07-17 01:20:50 -0400

    add username to from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,16 @@ class User < ApplicationRecord
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
 
+  scope :charity_runs, -> { joins(:runs).group(:charity_id) }
+
   has_secure_password
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
-      user.username = auth.info.name
+      user.user_name = auth.info.name
       user.email = auth.info.email
       user.password = SecureRandom.hex
     end
   end
+
 end
