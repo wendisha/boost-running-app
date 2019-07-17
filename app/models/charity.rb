b180ed0bd30dba685eb5fd02741a8899330e15af
sarahpaz <sarah.paz@gmail.com>
2019-07-17 01:19:28 -0400

    remove error method

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,12 +1,11 @@
 class Charity < ApplicationRecord
+
   has_many :runs
   has_many :users, through: :runs
 
   validates :name, :description, presence: true
   validate :not_a_duplicate, on: :create
-  
-  def not_a_duplicate
-    errors.add(:name, "is already a listed charity.") if Charity.find_by(name: name)
 
-  end
 end
+
+
