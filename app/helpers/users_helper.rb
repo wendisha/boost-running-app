5cc02cd82f4c84ecd963c659adedab30ac01edcd
sarahpaz <sarah.paz@gmail.com>
2019-07-18 15:17:28 -0400

    add display welcome message method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  def display_welcome_message
+    if @user == current_user 
+      tag.h2("Welcome, #{@user.username.capitalize}!")
+    else
+      tag.h2("Run History: #{@user.username.capitalize}")
+    end
+  end
+
 end
