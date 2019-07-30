a8f11faed9e4c72e8bc773eb8202386dbd784d45
sarahpaz <sarah.paz@gmail.com>
2019-07-30 15:41:32 -0400

    add user_delete_run helper method

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -1,2 +1,11 @@
 module RunsHelper
+
+  def user_delete_run
+    if @run.user_id == current_user.id 
+      
+     link_to 'Edit', edit_run_path | link_to "Delete", run_path(@run), method: :delete, data: { confirm: 'Are you sure you want to delete this run?'} 
+    
+    end
+  end
 end
+
