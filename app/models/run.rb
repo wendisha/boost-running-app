9b388b903aa55c8d69f95a3bd3f33e03fc1ea3a6
sarahpaz <sarah.paz@gmail.com>
2019-07-29 21:06:33 -0400

    add total_distance and total_amount_raised scope methods

--- app/models/run.rb
+++ app/models/run.rb
@@ -10,4 +10,12 @@ class Run < ApplicationRecord
     self.charity
   end
 
+  def self.total_distance(user)
+    where("user_id = ?", user.id).inject(0) { |sum, r| sum + r.distance } 
+  end
+
+  def self.total_amount_raised(user)
+    where("user_id = ?", user.id).inject(0) { |sum, r| sum + r.distance * 3 }
+  end
+  
 end
